1a1ef23f0acc74f45213e98f95686b791348e273
Alexey <alexeyAYAYA@gmail.com>
2021-08-09 15:45:15 +0700

    Update mediapipe_video2skelet.py

--- app/mediapipe_video2skelet.py
+++ app/mediapipe_video2skelet.py
@@ -6,7 +6,7 @@ sys.path.append('../slsru_skelet')
 from slsru_skelet.skelet_mediapipe import MediapipeModel_v08
 
 # data_folder = "data/video/"
-data_folder = "D:/git/GitHub/YAYAYru/slsru_skelet/data/video/"
+data_folder = "data/video/"
 # filename = data_folder + "S1540022.mp4"
 filename = data_folder + "12_s10020_9_1.mp4"
 
