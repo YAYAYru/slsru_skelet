02f84ace09c6afde501af34f11c6394a8371ff51
Alexey <alexeyAYAYA@gmail.com>
2021-08-09 15:02:46 +0700

    Create mediapipe_video2skelet.py

---
+++ app/mediapipe_video2skelet.py
@@ -0,0 +1,16 @@
+import numpy as np
+import cv2 as cv
+import sys
+sys.path.append('../slsru_skelet')
+
+from slsru_skelet.skelet_mediapipe import MediapipeModel_v08
+
+# data_folder = "data/video/"
+data_folder = "D:/git/GitHub/YAYAYru/slsru_skelet/data/video/"
+# filename = data_folder + "S1540022.mp4"
+filename = data_folder + "12_s10020_9_1.mp4"
+
+s_m = MediapipeModel_v08(filename, 30, 30, "full")
+s_m.processing()
+s_m.show(mode=1)
+s_m.to_csv(filename + ".csv")
