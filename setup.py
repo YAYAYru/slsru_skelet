ac713761253a6d1f5627958ca65f2b5a19a37d95
YAYAYru <AlexeyAYAYA@gmail.com>
2022-09-19 15:59:34 +0700

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="slsru_skelet",
+        version="0.1.0",
+        description="Work with skeletal model",
+        author="Alexey Prikhodko",
+        packages=["slsru_skelet"],
+        zip_safe=False)
